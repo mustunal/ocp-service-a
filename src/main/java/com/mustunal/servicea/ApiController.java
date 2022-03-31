2a3f5a370748e746e5998b6889a6c2edb9a633e5
TKiccteam <iccteam@thy.com>
2022-03-31 22:48:28 +0300

    small change

--- src/main/java/com/mustunal/servicea/ApiController.java
+++ src/main/java/com/mustunal/servicea/ApiController.java
@@ -1,15 +1,28 @@
 package com.mustunal.servicea;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ApiController {
 
+
+    @Value("${service-b-url}")
+    private String serviceBUrl;
+    @Value("${service-b-port}")
+    private String serviceBPort;
+
     @GetMapping
     public String sayHello(){
-        return "Hello World!";
+
+        String requestUrl = "http://"+serviceBUrl+":"+serviceBPort+"/api/v1";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String forObject = restTemplate.getForObject(requestUrl, String.class);
+        return "Hello World! And " + forObject;
     }
 }
